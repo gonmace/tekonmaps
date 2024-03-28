fdb702cff0a9cf3389700a2f29f08723f04581b6
magoreal4 <magoreal4@gmail.com>
2024-03-28 14:03:42 -0300

    first commit

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -37,3 +37,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://pti.tekon-rl.cl']
